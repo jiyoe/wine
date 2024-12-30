58338b33d6b77e490d641e002c55c969ef925d70
jiyoe <ejdgodl@naver.com>
2024-12-30 09:31:50 +0900

    Initial Commit

---
+++ wineTests/wineTests.swift
@@ -0,0 +1,17 @@
+//
+//  wineTests.swift
+//  wineTests
+//
+//  Created by jy on 12/30/24.
+//
+
+import Testing
+@testable import wine
+
+struct wineTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
